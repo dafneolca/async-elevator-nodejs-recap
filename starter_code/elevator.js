06e353ff4d1fe5b6cb207e497908007fbcd4e5d1
Dafne <ddolca@github.com>
2018-10-29 17:53:09 +0100

    Completed iteration 2

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,19 +1,43 @@
+/*jshint esversion: 6*/
 class Elevator {
-  constructor(){
-    this.floor      = 0;
-    this.MAXFLOOR   = 10;
-    this.requests   = [];
-  }
+    constructor(floor, MAXFLOOR, requests) {
+        this.floor = 0;
+        this.MAXFLOOR = 10;
+        this.requests = [];
+    }
 
-  start() { }
-  stop() { }
-  update() { }
-  _passengersEnter() { }
-  _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
+    start() {
+        setInterval(() => update(), 1000);
+    }
+
+    stop() {
+        clearInterval(start.setInterval);
+    }
+
+    update() {
+        this.log();
+    }
+    _passengersEnter() {}
+    _passengersLeave() {}
+
+    floorUp() {
+        if (this.floor < this.MAXFLOOR) {
+            this.floor ++;
+        }
+        console.log(this.floor);
+    }
+    // floorUp(floor) {     if (floor < Elevator.MAXFLOOR) {         floor += 1; } }
+    floorDown() {
+        if (Elevator.floor > 0) {
+            Elevator.floor -= 1;
+        }
+        console.log(Elevator.floor);
+    }
+    // floorDown(floor) {     if (floor > 0) {         floor -= 1;     } }
+    call() {}
+    log(direction, floor) {
+        console.log(`Direction: ${direction} | Floor: ${floor}`);
+    }
 }
 
 module.exports = Elevator;
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,1 +1,8 @@
+/*jshint esversion: 6*/
 const Elevator = require('./elevator.js');
+const Person = require('./person.js');
+
+let elevator = new Elevator();
+
+elevator.floorUp();
+
